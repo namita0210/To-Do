cf9e495d7257d1ca0bf122c43d76e072c77549c2
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-05-03 15:34:57 +0530

    import the class name from the view.py
    and mofify the mapping in the path

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import TaskList
 
 urlpatterns = [
-    path('' , views.taskList, name="tasks" )   
+    path('' , TaskList.as_view(), name="tasks" )   
 ]
