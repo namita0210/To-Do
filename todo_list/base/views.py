90be4891a3421fdb36e7e9bc9267a3231b7a28ad
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-05-03 16:25:32 +0530

    add taskDetail class that looks for a template task_detail.html in the templates

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Task
+from django.views.generic.detail import DetailView
 # Create your views here.
 
 class TaskList(ListView):
     model= Task
     context_object_name = 'tasks'
 
+class TaskDetail(DetailView):
+    model = Task
+    
