1f7abb50c3fdf8af072a2968c68b0eeadf3804b6
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-05-04 12:00:36 +0530

    add a class for creation of new item in 'views.py'

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Task
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 # Create your views here.
 
 class TaskList(ListView):
@@ -12,3 +14,9 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    field = '__all__' #all the columns specified in the table in models.py
+    success_url = reverse_lazy('tasks')
+        
