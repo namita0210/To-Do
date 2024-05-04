2b4b28271e7233789860935795ccf4131aef8190
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-05-04 14:24:05 +0530

    add mapping to login class for the url

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    
+    path('login/' , CustomLoginView, name="login" ),
     path('' , TaskList.as_view(), name="tasks" ) ,
     path('task/<int:pk>' , TaskDetail.as_view(), name="task" ),
     path('task-create/' , TaskCreate.as_view(), name="task-create"),
