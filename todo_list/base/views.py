2fe77b19bf62bb295a71304848ab2c68f9470e69
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-05-04 14:24:29 +0530

    add class for login in the views

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -4,7 +4,16 @@ from .models import Task
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model= Task
@@ -26,6 +35,10 @@ class TaskUpdate(UpdateView):
    success_url = reverse_lazy('tasks') 
 
 class TaskDelete(DeleteView):
+
     model=Task
     context_object_name="task"
     success_url = reverse_lazy('tasks')
+
+
+
