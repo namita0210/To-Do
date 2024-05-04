58fcfd249071bf52567c5bf75a28898fa927e63d
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-05-04 12:56:38 +0530

    map task update class to urls

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -5,7 +5,8 @@ from .views import *
 urlpatterns = [
     path('' , TaskList.as_view(), name="tasks" ) ,
     path('task/<int:pk>' , TaskDetail.as_view(), name="task" ),
-    path('task-create/' , TaskCreate.as_view(), name="task-create") 
+    path('task-create/' , TaskCreate.as_view(), name="task-create"),
+    path('task-update/<int:pk>' , TaskUpdate.as_view(), name="task-update" ) 
 
 
 ]
